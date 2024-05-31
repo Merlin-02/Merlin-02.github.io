104d75d3e069323e62f5474f7cdfa8d21134ef05
Merlin-02 <kcabrerac2200@alumno.ipn.mx>
2024-05-31 00:20:05 -0600

    Creacion de actividades
    Se añadio un carrusell a Actividades el cual puede ser util para colocar las imagenes, una descripcion y redirigirnos a algun lugar
    
    Revisalo @Rackzo-AyEl

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /*Codigo para la galeria */
 
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
